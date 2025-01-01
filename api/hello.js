9334192e28c27b9ee04c34fb15121b96fdc7ee07
wydzki <mianshahirabd8@gmail.com>
2024-12-31 23:21:33 -0500

    updated

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,16 @@
-module.exports = (req, res) => {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log(`logged a niggas ip address: ${ip}`); // Log the IP to Vercel logs
+const axios = require('axios');
 
-    res.status(200).send('HI wsg wsp sup');
+module.exports = async (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log(`logged a niggas ip address: ${ip}`); 
+
+    try {
+        const response = await axios.get(`https://ipinfo.io/${ip}?token=YOUR_IPINFO_TOKEN`);
+        const { country, region, city } = response.data;
+
+        res.status(200).send(`he's from: ${city}, ${region}, ${country}`);
+    } catch (error) {
+        console.error('couldnt get bros region', error);
+        res.status(500).send('couldnt get bros region');
+    }
 };
