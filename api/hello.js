0e2cbeb0a1ac74c817eeb090fe655fe865c84bc4
wydzki <mianshahirabd8@gmail.com>
2025-01-05 10:31:52 -0500

    Hi

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log(`logged a nigga's IP address: ${ip}`); /
+    console.log(`logged a nigga's IP address: ${ip}`); // bruh dont even
 
     res.status(200).send('HI wsg wsp sup');
 };
