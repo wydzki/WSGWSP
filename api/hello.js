6264e28ef314fc4827d6d39eb36c2477522d9c55
wydzki <mianshahirabd8@gmail.com>
2025-03-24 22:24:44 -0400

    updated

--- api/hello.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log(`logged a nigga's IP address: ${ip}`); // Log the IP to Vercel logs
-
-    res.status(200).send('HI wsg wsp sup');
-};
