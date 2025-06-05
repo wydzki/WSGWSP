2bd53b27e4f2d5d27e38bc9c4c51d0a8a2467cb5
wydzki <mianshahirabd8@gmail.com>
2025-06-05 02:16:20 -0400

    updated

--- api/uh oh.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log(`a person has pressed your website, new traffic!: ${ip}`); // bruh dont even
-
-    res.status(200).send(' ');
-};
